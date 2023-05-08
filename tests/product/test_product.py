a8ece27363386b1bdc7b7fc6154981d7d7130dac
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-05-08 16:21:50 -0300

    feat: req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  
+    product = Product(
+        id=1,
+        nome_do_produto="Heineken",
+        nome_da_empresa="Zé Birita",
+        data_de_fabricacao="08/05/2023",
+        data_de_validade="08/05/2024",
+        numero_de_serie=1000,
+        instrucoes_de_armazenamento="Frezzer",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Heineken"
+    assert product.nome_da_empresa == "Zé Birita"
+    assert product.data_de_fabricacao == "08/05/2023"
+    assert product.data_de_validade == " 08/05/2024"
+    assert product.numero_de_serie == 1000
+    assert product.instrucoes_de_armazenamento == "Frezzer"
